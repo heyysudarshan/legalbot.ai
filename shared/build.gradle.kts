1e244c7463403958e22ac27d17546b18ae023ca6
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-08-22 15:02:38 +0530

    Setup shared module with Kotlin multiplatform setup

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -1,5 +1,36 @@
 plugins {
+    alias(libs.plugins.kotlin.multiplatform)
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.compose.multiplatform)
+    alias(libs.plugins.compose.compiler)
+}
 
+group = libs.versions.shared.group.get()
+version = libs.versions.shared.version.get()
+
+kotlin {
+    androidTarget()
+
+    jvm("desktop")
+
+    iosX64()
+    iosArm64()
+    iosSimulatorArm64()
+
+    sourceSets {
+        commonMain.dependencies {
+            implementation(libs.compose.multiplatform.ui)
+        }
+    }
+}
+
+android {
+    namespace = libs.versions.shared.androidLibrary.namespace.get()
+    compileSdk = libs.versions.shared.androidLibrary.compileSdk.get().toInt()
+
+    defaultConfig {
+        minSdk = libs.versions.shared.androidLibrary.minSdk.get().toInt()
+    }
 }
 
 // Custom build directory
