45e70adb7776203d3fe6d1899dd1f9c9bf60ea55
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-08-22 19:23:52 +0530

    Define android-app module's configuration & android app package information from the version catalog

--- android-app/build.gradle.kts
+++ android-app/build.gradle.kts
@@ -4,5 +4,18 @@ plugins {
     alias(libs.plugins.compose.compiler)
 }
 
+android {
+    namespace = libs.versions.androidApp.androidApp.namespace.get()
+    compileSdk = libs.versions.androidApp.androidApp.compileSdk.get().toInt()
+
+    defaultConfig {
+        applicationId = libs.versions.androidApp.androidApp.applicationId.get()
+        minSdk = libs.versions.androidApp.androidApp.minSdk.get().toInt()
+        targetSdk = libs.versions.androidApp.androidApp.targetSdk.get().toInt()
+        versionCode = libs.versions.androidApp.androidApp.versionCode.get().toInt()
+        versionName = libs.versions.androidApp.androidApp.versionName.get()
+    }
+}
+
 // Custom build directory
 layout.buildDirectory.set(file(path = "$rootDir/.build/AndroidApp"))
