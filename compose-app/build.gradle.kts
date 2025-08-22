dc4d4d024c3b65625af2f4c9e91fc3fbe4dbd61f
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-08-22 14:36:21 +0530

    Apply the "org.jetbrains.compose" plugin to the compose-app module

--- compose-app/build.gradle.kts
+++ compose-app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.kotlin.multiplatform)
     alias(libs.plugins.android.library)
+    alias(libs.plugins.compose.multiplatform)
 }
 
 group = libs.versions.composeApp.group.get()
