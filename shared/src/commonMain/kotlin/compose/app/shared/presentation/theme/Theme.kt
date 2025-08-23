587722ac3cc2a342cd8e43a7bbb3b49cfc8eb99d
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-08-23 18:42:14 +0530

    Add `Theme.kt` to define ComposeApp theme with light and dark color schemes

---
+++ shared/src/commonMain/kotlin/compose/app/shared/presentation/theme/Theme.kt
@@ -0,0 +1,25 @@
+package compose.app.shared.presentation.theme
+
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.runtime.Composable
+
+private val lightColorScheme = lightColorScheme()
+
+private val darkColorScheme = darkColorScheme()
+
+@Composable
+fun ComposeAppTheme(
+    isDarkTheme: Boolean = false,
+    content: @Composable () -> Unit
+) {
+    val colorScheme = if (isDarkTheme) darkColorScheme else lightColorScheme
+    val typography = getTypography()
+
+    MaterialTheme(
+        colorScheme = colorScheme,
+        typography = typography,
+        content = content
+    )
+}
