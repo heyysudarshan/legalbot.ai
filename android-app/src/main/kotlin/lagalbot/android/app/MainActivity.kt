54bba9bb4ff77104439cae0b99968ac2e271ce26
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-08-23 11:10:25 +0530

    Create `MainActivity` in the android-app module

---
+++ android-app/src/main/kotlin/lagalbot/android/app/MainActivity.kt
@@ -0,0 +1,16 @@
+package lagalbot.android.app
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+
+        }
+    }
+}
