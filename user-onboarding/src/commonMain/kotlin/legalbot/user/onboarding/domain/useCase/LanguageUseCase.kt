7b1180285ec8308457a31eb4660ab23694de694b
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-08-24 00:13:03 +0530

    Add `LanguageUseCase` to handle language retrieval and selection in the onboarding domain

---
+++ user-onboarding/src/commonMain/kotlin/legalbot/user/onboarding/domain/useCase/LanguageUseCase.kt
@@ -0,0 +1,17 @@
+package legalbot.user.onboarding.domain.useCase
+
+import kotlinx.coroutines.flow.StateFlow
+import legalbot.user.onboarding.domain.model.Language
+import legalbot.user.onboarding.domain.repository.LanguageRepository
+
+internal class LanguageUseCase(
+    private val repository: LanguageRepository
+) {
+    fun getLanguages(): StateFlow<List<Language>> {
+        return repository.getLanguages()
+    }
+
+    fun updateSelectedLanguage(language: Language) {
+        repository.updateSelectedLanguage(language = language)
+    }
+}
