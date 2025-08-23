1ff98c44ff04a4636d6e2caea117dc9c55babc82
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-08-24 00:36:35 +0530

    Initialize Koin with `userOnboardingModule` in `ComposeApplication` for dependency injection setup

--- android-app/src/main/kotlin/legalbot/android/app/ComposeApplication.kt
+++ android-app/src/main/kotlin/legalbot/android/app/ComposeApplication.kt
@@ -1,9 +1,18 @@
 package legalbot.android.app
 
 import android.app.Application
+import legalbot.user.onboarding.di.userOnboardingModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
 
 class ComposeApplication : Application() {
     override fun onCreate() {
         super.onCreate()
+        startKoin {
+            androidContext(this@ComposeApplication)
+            modules(
+                userOnboardingModule
+            )
+        }
     }
 }
