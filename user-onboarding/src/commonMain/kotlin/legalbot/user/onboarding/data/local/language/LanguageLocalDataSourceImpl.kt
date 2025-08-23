6baaedee85fc187df55f4306ff0fad0476e8750f
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-08-24 00:00:42 +0530

    Implement `LanguageLocalDataSourceImpl` with language management and selection logic

---
+++ user-onboarding/src/commonMain/kotlin/legalbot/user/onboarding/data/local/language/LanguageLocalDataSourceImpl.kt
@@ -0,0 +1,32 @@
+package legalbot.user.onboarding.data.local.language
+
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.update
+import legalbot.user.onboarding.domain.model.Language
+
+internal class LanguageLocalDataSourceImpl : LanguageLocalDataSource {
+    private val languageList = MutableStateFlow(
+        listOf(
+            Language(name = "English", message = "Use this app in English.", locale = "en", isSelected = true),
+            Language(name = "Marathi", message = "हे अ‍ॅप मराठीत वापरा.", locale = "mr", isSelected = false),
+            Language(name = "Hindi", message = "इस ऐप का उपयोग हिंदी में करें।", locale = "hi", isSelected = false),
+        )
+    )
+
+    override fun getLanguages(): StateFlow<List<Language>> {
+        return languageList
+    }
+
+    override fun updateSelectedLanguage(language: Language) {
+        languageList.update { value ->
+            value.map {
+                if (it == language) {
+                    language.copy(isSelected = true)
+                } else {
+                    it.copy(isSelected = false)
+                }
+            }
+        }
+    }
+}
