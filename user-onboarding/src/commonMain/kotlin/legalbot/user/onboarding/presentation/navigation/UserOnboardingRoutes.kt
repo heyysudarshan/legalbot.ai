b9308fb183c27e91d40494fd248c1a9619b45719
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-08-23 16:47:51 +0530

    Restrict `UserOnboardingRoutes` visibility to internal

--- user-onboarding/src/commonMain/kotlin/legalbot/user/onboarding/presentation/navigation/UserOnboardingRoutes.kt
+++ user-onboarding/src/commonMain/kotlin/legalbot/user/onboarding/presentation/navigation/UserOnboardingRoutes.kt
@@ -1,5 +1,5 @@
 package legalbot.user.onboarding.presentation.navigation
 
-sealed class UserOnboardingRoutes {
+internal sealed class UserOnboardingRoutes {
     object LanguagePage : UserOnboardingRoutes()
 }
