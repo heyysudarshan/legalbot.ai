99a4a2f323eaec658619c3e3920b88e23497c40a
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-08-23 23:44:25 +0530

    Add `isSelected` property to `Language` data model

--- user-onboarding/src/commonMain/kotlin/legalbot/user/onboarding/domain/model/Language.kt
+++ user-onboarding/src/commonMain/kotlin/legalbot/user/onboarding/domain/model/Language.kt
@@ -3,5 +3,6 @@ package legalbot.user.onboarding.domain.model
 internal data class Language(
     val name: String,
     val message: String,
-    val locale: String
+    val locale: String,
+    val isSelected: Boolean,
 )
