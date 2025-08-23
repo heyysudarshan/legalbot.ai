909c2163500181c0d74d3cfef18d6193a8d026a6
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-08-23 23:56:39 +0530

    Rename `LocalLanguageDataSource` to `LanguageLocalDataSource` and update method names for clarity

--- user-onboarding/src/commonMain/kotlin/legalbot/user/onboarding/data/local/language/LanguageLocalDataSource.kt
+++ user-onboarding/src/commonMain/kotlin/legalbot/user/onboarding/data/local/language/LanguageLocalDataSource.kt
@@ -3,7 +3,7 @@ package legalbot.user.onboarding.data.local.language
 import kotlinx.coroutines.flow.StateFlow
 import legalbot.user.onboarding.domain.model.Language
 
-internal interface LocalLanguageDataSource {
-    fun getLanguageList(): StateFlow<List<Language>>
+internal interface LanguageLocalDataSource {
+    fun getLanguages(): StateFlow<List<Language>>
     fun updateSelectedLanguage(language: Language)
 }
