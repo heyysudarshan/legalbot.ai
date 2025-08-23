4ea9307410451088e1c3bf71ae50e4b99358d8a1
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-08-23 10:24:01 +0530

    Add Kotlin Multiplatform and Android library configuration to user-onboarding module and enable Compose support

--- user-onboarding/build.gradle.kts
+++ user-onboarding/build.gradle.kts
@@ -1,6 +1,36 @@
 plugins {
-
+    alias(libs.plugins.kotlin.multiplatform)
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.compose.multiplatform)
+    alias(libs.plugins.compose.compiler)
 }
 
+group = libs.versions.userOnboarding.group.get()
+version = libs.versions.userOnboarding.version.get()
+
+kotlin {
+    androidTarget()
+
+    jvm("desktop")
+
+    iosX64()
+    iosArm64()
+    iosSimulatorArm64()
+
+    sourceSets {
+        commonMain.dependencies {
+            implementation(libs.compose.multiplatform.ui)
+        }
+    }
+}
+
+android {
+    namespace = libs.versions.userOnboarding.androidLibrary.namespace.get()
+    compileSdk = libs.versions.userOnboarding.androidLibrary.compileSdk.get().toInt()
+
+    defaultConfig {
+        minSdk = libs.versions.userOnboarding.androidLibrary.minSdk.get().toInt()
+    }
+}
 // Custom build directory
 layout.buildDirectory.set(file(path = "$rootDir/.build/UserOnboarding"))
