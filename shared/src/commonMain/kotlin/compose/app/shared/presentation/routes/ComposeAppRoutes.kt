d245ecb5903c29f6440d921d30b1f8c533b1e750
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-08-24 02:06:22 +0530

    Annotate `UserOnboardingModule` route with `@Serializable` to enable serialization.

--- shared/src/commonMain/kotlin/compose/app/shared/presentation/routes/ComposeAppRoutes.kt
+++ shared/src/commonMain/kotlin/compose/app/shared/presentation/routes/ComposeAppRoutes.kt
@@ -1,5 +1,8 @@
 package compose.app.shared.presentation.routes
 
+import kotlinx.serialization.Serializable
+
 sealed class ComposeAppRoutes {
+    @Serializable
     object UserOnboardingModule : ComposeAppRoutes()
 }
