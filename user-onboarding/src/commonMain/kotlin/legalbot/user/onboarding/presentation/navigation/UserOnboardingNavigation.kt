e98f452c9ce24d33bd4598947a72577a78ddc806
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-08-23 16:48:33 +0530

    Add `UserOnboardingNavigation` composable for onboarding navigation flow

---
+++ user-onboarding/src/commonMain/kotlin/legalbot/user/onboarding/presentation/navigation/UserOnboardingNavigation.kt
@@ -0,0 +1,23 @@
+package legalbot.user.onboarding.presentation.navigation
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import legalbot.user.onboarding.presentation.languagePage.LanguagePage
+
+@Composable
+fun UserOnboardingNavigation() {
+    val userOnboardingNavController = rememberNavController()
+
+    NavHost(
+        navController = userOnboardingNavController,
+        startDestination = UserOnboardingRoutes.LanguagePage
+    ) {
+        composable<UserOnboardingRoutes.LanguagePage> {
+            LanguagePage(modifier = Modifier.fillMaxSize())
+        }
+    }
+}
