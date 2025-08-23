97069bf9fab82b02e6508c2a4668d9d8878c0701
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-08-23 23:46:27 +0530

    Add `LanguagePageUiState` data class to represent state for language selection page

---
+++ user-onboarding/src/commonMain/kotlin/legalbot/user/onboarding/presentation/languagePage/LanguagePageUiState.kt
@@ -0,0 +1,7 @@
+package legalbot.user.onboarding.presentation.languagePage
+
+import legalbot.user.onboarding.domain.model.Language
+
+internal data class LanguagePageUiState(
+    val languages: List<Language> = emptyList(),
+)
