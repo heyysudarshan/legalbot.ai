b1268013774976342e79fdcb5474823e8d3a28c3
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-08-24 02:00:50 +0530

    Refactor `userOnboardingModule` to use `singleOf` and `viewModelOf` for cleaner Koin dependency declarations

--- user-onboarding/src/commonMain/kotlin/legalbot/user/onboarding/di/userOnboardingModule.kt
+++ user-onboarding/src/commonMain/kotlin/legalbot/user/onboarding/di/userOnboardingModule.kt
@@ -1,14 +1,19 @@
 package legalbot.user.onboarding.di
 
+import legalbot.user.onboarding.data.local.language.LanguageLocalDataSource
 import legalbot.user.onboarding.data.local.language.LanguageLocalDataSourceImpl
 import legalbot.user.onboarding.data.repository.LanguageRepositoryImpl
+import legalbot.user.onboarding.domain.repository.LanguageRepository
 import legalbot.user.onboarding.domain.useCase.LanguageUseCase
 import legalbot.user.onboarding.presentation.languagePage.LanguagePageViewModel
+import org.koin.core.module.dsl.singleOf
+import org.koin.core.module.dsl.viewModelOf
+import org.koin.dsl.bind
 import org.koin.dsl.module
 
 val userOnboardingModule = module {
-    single { LanguageLocalDataSourceImpl() }
-    single { LanguageRepositoryImpl(get()) }
-    single { LanguageUseCase(get()) }
-    single { LanguagePageViewModel(get()) }
+    singleOf(::LanguageLocalDataSourceImpl).bind<LanguageLocalDataSource>()
+    singleOf(::LanguageRepositoryImpl).bind<LanguageRepository>()
+    singleOf(::LanguageUseCase)
+    viewModelOf(::LanguagePageViewModel)
 }
