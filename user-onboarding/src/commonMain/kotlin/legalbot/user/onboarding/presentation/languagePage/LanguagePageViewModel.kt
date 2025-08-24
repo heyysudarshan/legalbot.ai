e840f81b3316ada59c9d007b4cf9b7801b4afd4b
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-08-24 09:32:31 +0530

    Remove unused `animationJob` and related logic from `LanguagePageViewModel`. Refactor `uiState` updates for clarity.

--- user-onboarding/src/commonMain/kotlin/legalbot/user/onboarding/presentation/languagePage/LanguagePageViewModel.kt
+++ user-onboarding/src/commonMain/kotlin/legalbot/user/onboarding/presentation/languagePage/LanguagePageViewModel.kt
@@ -2,8 +2,6 @@ package legalbot.user.onboarding.presentation.languagePage
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import kotlinx.coroutines.Job
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
@@ -16,12 +14,10 @@ internal class LanguagePageViewModel(
     var uiState = MutableStateFlow(LanguagePageUiState())
         private set
 
-    private var animationJob: Job? = null
-
     init {
         viewModelScope.launch {
             languageUseCase.getLanguages().collect { languages ->
-                uiState.collect { value ->
+                uiState.update { value ->
                     value.copy(languages = languages)
                 }
             }
@@ -30,21 +26,5 @@ internal class LanguagePageViewModel(
 
     fun updateSelectedLanguage(language: Language) {
         languageUseCase.updateSelectedLanguage(language = language)
-    }
-
-    fun startAnimationJob() {
-        animationJob = viewModelScope.launch {
-            val totalLanguagesCount = uiState.value.languages.size
-            var animationJobProgress = 0L
-
-            repeat(times = totalLanguagesCount) {
-                uiState.update { value ->
-                    animationJobProgress += 100L
-                    value.copy(animationJobProgress = animationJobProgress)
-                }
-                delay(100L)
-            }
-        }
-        animationJob?.cancel()
     }
 }
