b1096c1372c0481bb7df4ea6d00c974b96758326
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-08-24 08:46:02 +0530

    Add `animationJobProgress` property to `LanguagePageUiState` for handling animation progress

--- user-onboarding/src/commonMain/kotlin/legalbot/user/onboarding/presentation/languagePage/LanguagePageUiState.kt
+++ user-onboarding/src/commonMain/kotlin/legalbot/user/onboarding/presentation/languagePage/LanguagePageUiState.kt
@@ -4,4 +4,5 @@ import legalbot.user.onboarding.domain.model.Language
 
 internal data class LanguagePageUiState(
     val languages: List<Language> = emptyList(),
+    val animationJobProgress: Long = 0L,
 )
