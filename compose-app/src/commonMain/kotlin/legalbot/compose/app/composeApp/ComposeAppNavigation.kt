db91de3ba3e4c084423cac7663564ae17907d11e
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-08-24 08:15:49 +0530

    Add `ComposeAppNavigation` to manage app-level navigation with `NavHost` and `UserOnboardingNavigation` integration

---
+++ compose-app/src/commonMain/kotlin/legalbot/compose/app/composeApp/ComposeAppNavigation.kt
@@ -0,0 +1,22 @@
+package legalbot.compose.app.composeApp
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import compose.app.shared.presentation.routes.ComposeAppRoutes
+import legalbot.user.onboarding.presentation.navigation.UserOnboardingNavigation
+
+@Composable
+fun ComposeAppNavigation() {
+    val composeAppNavController = rememberNavController()
+
+    NavHost(
+        navController = composeAppNavController,
+        startDestination = ComposeAppRoutes.UserOnboardingModule
+    ) {
+        composable<ComposeAppRoutes.UserOnboardingModule> {
+            UserOnboardingNavigation()
+        }
+    }
+}
