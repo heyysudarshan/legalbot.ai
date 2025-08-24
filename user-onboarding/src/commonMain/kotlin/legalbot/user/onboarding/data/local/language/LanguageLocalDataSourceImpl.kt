9e610596000742f23e257bbbfe2fcd34663db59c
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-08-24 08:41:00 +0530

    Format `Language` objects in `LanguageLocalDataSourceImpl` for improved readability.

--- user-onboarding/src/commonMain/kotlin/legalbot/user/onboarding/data/local/language/LanguageLocalDataSourceImpl.kt
+++ user-onboarding/src/commonMain/kotlin/legalbot/user/onboarding/data/local/language/LanguageLocalDataSourceImpl.kt
@@ -8,9 +8,24 @@ import legalbot.user.onboarding.domain.model.Language
 internal class LanguageLocalDataSourceImpl : LanguageLocalDataSource {
     private val languageList = MutableStateFlow(
         listOf(
-            Language(name = "English", message = "Use this app in English.", locale = "en", isSelected = true),
-            Language(name = "Marathi", message = "हे अ‍ॅप मराठीत वापरा.", locale = "mr", isSelected = false),
-            Language(name = "Hindi", message = "इस ऐप का उपयोग हिंदी में करें।", locale = "hi", isSelected = false),
+            Language(
+                name = "English",
+                message = "Use this app in English.",
+                locale = "en",
+                isSelected = true
+            ),
+            Language(
+                name = "Marathi",
+                message = "हे अ‍ॅप मराठीत वापरा.",
+                locale = "mr",
+                isSelected = false
+            ),
+            Language(
+                name = "Hindi",
+                message = "इस ऐप का उपयोग हिंदी में करें।",
+                locale = "hi",
+                isSelected = false
+            ),
         )
     )
 
