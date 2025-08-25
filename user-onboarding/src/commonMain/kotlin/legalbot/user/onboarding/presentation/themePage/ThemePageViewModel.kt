d939608aaa961f5325e190c74174d8e60271bf64
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-08-25 16:04:04 +0530

    Add `ThemePageViewModel` to manage theme data and UI state in onboarding flow

---
+++ user-onboarding/src/commonMain/kotlin/legalbot/user/onboarding/presentation/themePage/ThemePageViewModel.kt
@@ -0,0 +1,22 @@
+package legalbot.user.onboarding.presentation.themePage
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+import legalbot.user.onboarding.domain.useCase.ThemeUseCase
+
+internal class ThemePageViewModel(private val themeUseCase: ThemeUseCase) : ViewModel() {
+    val uiState = MutableStateFlow(ThemePageUiState())
+
+    init {
+        viewModelScope.launch {
+            themeUseCase.getThemes().collect { themes ->
+                uiState.update { value ->
+                    value.copy(themes = themes)
+                }
+            }
+        }
+    }
+}
