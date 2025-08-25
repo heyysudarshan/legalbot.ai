7e275d96adbaa477149931172eea8a6f6c1e608b
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-08-25 15:52:20 +0530

    Implement `ThemeRepository` interface in `ThemeRepositoryImpl` and update function overrides

--- user-onboarding/src/commonMain/kotlin/legalbot/user/onboarding/data/repository/ThemeRepositoryImpl.kt
+++ user-onboarding/src/commonMain/kotlin/legalbot/user/onboarding/data/repository/ThemeRepositoryImpl.kt
@@ -3,15 +3,16 @@ package legalbot.user.onboarding.data.repository
 import kotlinx.coroutines.flow.StateFlow
 import legalbot.user.onboarding.data.local.theme.ThemeLocalDataSource
 import legalbot.user.onboarding.domain.model.Theme
+import legalbot.user.onboarding.domain.repository.ThemeRepository
 
 internal class ThemeRepositoryImpl(
     private val localDataSource: ThemeLocalDataSource
-) {
-    fun getThemes(): StateFlow<List<Theme>> {
+): ThemeRepository {
+    override fun getThemes(): StateFlow<List<Theme>> {
         return localDataSource.getThemes()
     }
 
-    fun updateSelectedTheme(theme: Theme) {
+    override fun updateSelectedTheme(theme: Theme) {
         localDataSource.updateSelectedTheme(theme = theme)
     }
 }
