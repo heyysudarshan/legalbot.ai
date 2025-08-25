84cd28308b9d348763209c0d80124fe0c05a60b2
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-08-25 16:02:22 +0530

    Add dependency injections for theme data sources, repository, use case, and view model

--- user-onboarding/src/commonMain/kotlin/legalbot/user/onboarding/di/userOnboardingModule.kt
+++ user-onboarding/src/commonMain/kotlin/legalbot/user/onboarding/di/userOnboardingModule.kt
@@ -2,10 +2,16 @@ package legalbot.user.onboarding.di
 
 import legalbot.user.onboarding.data.local.language.LanguageLocalDataSource
 import legalbot.user.onboarding.data.local.language.LanguageLocalDataSourceImpl
+import legalbot.user.onboarding.data.local.theme.ThemeLocalDataSource
+import legalbot.user.onboarding.data.local.theme.ThemeLocalDataSourceImpl
 import legalbot.user.onboarding.data.repository.LanguageRepositoryImpl
+import legalbot.user.onboarding.data.repository.ThemeRepositoryImpl
 import legalbot.user.onboarding.domain.repository.LanguageRepository
+import legalbot.user.onboarding.domain.repository.ThemeRepository
 import legalbot.user.onboarding.domain.useCase.LanguageUseCase
+import legalbot.user.onboarding.domain.useCase.ThemeUseCase
 import legalbot.user.onboarding.presentation.languagePage.LanguagePageViewModel
+import legalbot.user.onboarding.presentation.themePage.ThemePageViewModel
 import org.koin.core.module.dsl.singleOf
 import org.koin.core.module.dsl.viewModelOf
 import org.koin.dsl.bind
@@ -16,4 +22,9 @@ val userOnboardingModule = module {
     singleOf(::LanguageRepositoryImpl).bind<LanguageRepository>()
     singleOf(::LanguageUseCase)
     viewModelOf(::LanguagePageViewModel)
+
+    singleOf(::ThemeLocalDataSourceImpl).bind<ThemeLocalDataSource>()
+    singleOf(::ThemeRepositoryImpl).bind<ThemeRepository>()
+    singleOf(::ThemeUseCase)
+    viewModelOf(::ThemePageViewModel)
 }
