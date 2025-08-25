6ceb2305149918d4a788d72cb0e096e8289d5499
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-08-25 13:52:54 +0530

    Wrap `ComposeApp` content in `ComposeAppTheme` and `Scaffold`.

--- compose-app/src/commonMain/kotlin/legalbot/compose/app/ComposeApp.kt
+++ compose-app/src/commonMain/kotlin/legalbot/compose/app/ComposeApp.kt
@@ -1,8 +1,25 @@
 package legalbot.compose.app
 
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import compose.app.shared.presentation.theme.ComposeAppTheme
+import legalbot.compose.app.composeApp.ComposeAppNavigation
 
 @Composable
 internal fun ComposeApp() {
+    ComposeAppTheme {
+        Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+            val backgroundModifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding)
 
+            Surface(modifier = backgroundModifier) {
+                ComposeAppNavigation()
+            }
+        }
+    }
 }
