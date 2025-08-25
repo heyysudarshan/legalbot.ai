991dd1e8e74516ab21a08295cd68abb25b10028d
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-08-25 15:44:15 +0530

    Add `LocalThemeDataSource` interface for theme data management in onboarding

---
+++ user-onboarding/src/commonMain/kotlin/legalbot/user/onboarding/data/local/theme/LocalThemeDataSourceImpl.kt
@@ -0,0 +1,47 @@
+package legalbot.user.onboarding.data.local.theme
+
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.update
+import legalbot.user.onboarding.domain.model.Theme
+import legalbot.user.onboarding.domain.model.ThemeType
+
+internal class LocalThemeDataSourceImpl : LocalThemeDataSource {
+    private val themeList = MutableStateFlow(
+        listOf(
+            Theme(
+                name = "System",
+                message = "Use the system theme.",
+                isSelected = true,
+                type = ThemeType.System
+            ),
+            Theme(
+                name = "Light",
+                message = "Use a light theme.",
+                isSelected = false,
+                type = ThemeType.Light
+            ),
+            Theme(
+                name = "Dark",
+                message = "Use a dark theme.",
+                isSelected = false,
+                type = ThemeType.Dark
+            )
+        )
+    )
+    override fun getThemes(): StateFlow<List<Theme>> {
+        return themeList
+    }
+
+    override fun updateSelectedTheme(theme: Theme) {
+        themeList.update { value ->
+            value.map {
+                if (it.type == theme.type) {
+                    theme.copy(isSelected = true)
+                } else {
+                    it.copy(isSelected = false)
+                }
+            }
+        }
+    }
+}
