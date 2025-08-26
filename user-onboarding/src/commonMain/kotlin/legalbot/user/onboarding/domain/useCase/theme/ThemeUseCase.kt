fbeced64fa76ac91f5234d24626f794bb646953a
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-08-26 22:29:52 +0530

    Refactor `LanguageUseCase` by inlining `repository` declaration

--- user-onboarding/src/commonMain/kotlin/legalbot/user/onboarding/domain/useCase/theme/ThemeUseCase.kt
+++ user-onboarding/src/commonMain/kotlin/legalbot/user/onboarding/domain/useCase/theme/ThemeUseCase.kt
@@ -4,10 +4,8 @@ import kotlinx.coroutines.flow.StateFlow
 import legalbot.user.onboarding.domain.model.theme.Theme
 import legalbot.user.onboarding.repository.theme.ThemeRepository
 
-internal class ThemeUseCase(
-    private val repository: ThemeRepository
-) {
-    suspend fun getThemes(): StateFlow<List<Theme>> {
+internal class ThemeUseCase(private val repository: ThemeRepository) {
+    fun getThemes(): StateFlow<List<Theme>> {
         return repository.getThemes()
     }
 
