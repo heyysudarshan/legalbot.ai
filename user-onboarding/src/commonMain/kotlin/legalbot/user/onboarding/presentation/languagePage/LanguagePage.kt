ac5234c4bf7f80931f9f73fcbfe8da6abfe2eb4f
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-08-26 23:26:58 +0530

    Add support for mobile landscape mode in `LanguagePage` layout

--- user-onboarding/src/commonMain/kotlin/legalbot/user/onboarding/presentation/languagePage/LanguagePage.kt
+++ user-onboarding/src/commonMain/kotlin/legalbot/user/onboarding/presentation/languagePage/LanguagePage.kt
@@ -3,17 +3,22 @@ package legalbot.user.onboarding.presentation.languagePage
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.adaptive.currentWindowAdaptiveInfo
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
-import androidx.navigation.NavController
+import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
+import androidx.window.core.layout.WindowHeightSizeClass
+import androidx.window.core.layout.WindowWidthSizeClass
 import compose.app.shared.presentation.theme.ComposeAppTheme
 import compose.app.shared.resources.Res
 import compose.app.shared.resources.language_page_heading
@@ -30,30 +35,87 @@ import org.koin.compose.viewmodel.koinViewModel
 
 @Composable
 internal fun LanguagePage(
-    userOnBoardingNavController: NavController,
+    userOnBoardingNavHostController: NavHostController,
     modifier: Modifier = Modifier
 ) {
     val languagePageViewModel = koinViewModel<LanguagePageViewModel>()
-    Column(modifier = modifier) {
+
+    val windowSizeClass = currentWindowAdaptiveInfo().windowSizeClass
+    val windowWidth = windowSizeClass.windowWidthSizeClass
+    val windowHeight = windowSizeClass.windowHeightSizeClass
+
+    // Check if the display layout is in mobile landscape mode
+    val isMobileLandscapeMode = windowWidth in listOf(
+        WindowWidthSizeClass.COMPACT,
+        WindowWidthSizeClass.MEDIUM,
+        WindowWidthSizeClass.EXPANDED
+    ) && windowHeight in listOf(WindowHeightSizeClass.COMPACT, WindowHeightSizeClass.MEDIUM)
+
+    if (isMobileLandscapeMode) {
+        Row(modifier = modifier) {
+            val modifier1 = Modifier
+                .fillMaxHeight()
+                .weight(0.8f)
+            val modifier2 = Modifier.weight(1.2f)
+
+            LandingPageLayout(
+                isMobileLandscapeMode = true,
+                languagePageViewModel = languagePageViewModel,
+                userOnBoardingNavHostController = userOnBoardingNavHostController,
+                modifier = modifier1,
+                modifier2 = modifier2,
+            )
+        }
+    } else {
+        Column(modifier = modifier) {
+            LandingPageLayout(
+                isMobileLandscapeMode = false,
+                languagePageViewModel = languagePageViewModel,
+                userOnBoardingNavHostController = userOnBoardingNavHostController,
+                modifier = Modifier.fillMaxWidth(),
+                modifier2 = Modifier.fillMaxWidth()
+            )
+        }
+    }
+}
+
+@Composable
+private fun LandingPageLayout(
+    isMobileLandscapeMode: Boolean,
+    languagePageViewModel: LanguagePageViewModel,
+    userOnBoardingNavHostController: NavHostController,
+    modifier: Modifier = Modifier,
+    modifier2: Modifier = Modifier,
+) {
+    Box(modifier = modifier) {
+        val backgroundModifier = if (isMobileLandscapeMode) {
+            Modifier
+                .headingAndMessageModifier(isMobileLandscapeMode = true)
+                .align(alignment = Alignment.Center)
+        } else {
+            Modifier.headingAndMessageModifier()
+        }
+
         HeadingAndMessage(
             heading = Res.string.language_page_heading,
             message = Res.string.language_page_message,
-            modifier = Modifier.headingAndMessageModifier()
+            modifier = backgroundModifier.align(alignment = Alignment.Center)
         )
-        Box(modifier = Modifier.fillMaxWidth()) {
-            LanguageList(
-                languagePageViewModel = languagePageViewModel,
-                modifier = Modifier.fillMaxSize()
-            )
-            BottomButtonAndContainer(
-                label = Res.string.next_button_label,
-                modifier = Modifier.bottomButtonAndContainerModifier()
-                    .align(alignment = Alignment.BottomEnd),
-                onClick = {
-                    userOnBoardingNavController.navigate(UserOnboardingRoutes.ThemPage)
-                }
-            )
-        }
+    }
+    Box(modifier = modifier2) {
+        LanguageList(
+            languagePageViewModel = languagePageViewModel,
+            modifier = Modifier.fillMaxSize()
+        )
+        BottomButtonAndContainer(
+            label = Res.string.next_button_label,
+            modifier = Modifier
+                .bottomButtonAndContainerModifier()
+                .align(alignment = Alignment.BottomEnd),
+            onClick = {
+                userOnBoardingNavHostController.navigate(UserOnboardingRoutes.ThemPage)
+            }
+        )
     }
 }
 
@@ -83,7 +145,7 @@ private fun LanguageList(
 private fun LanguagePagePreview() {
     ComposeAppTheme {
         LanguagePage(
-            userOnBoardingNavController = rememberNavController(),
+            userOnBoardingNavHostController = rememberNavController(),
             modifier = Modifier.fillMaxSize(),
         )
     }
