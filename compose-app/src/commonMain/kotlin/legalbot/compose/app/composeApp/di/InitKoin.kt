153baf8a53d1589327631297e7996126cb912c88
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-08-26 12:34:25 +0530

    Inject `sharedModule` in Koin initialization

--- compose-app/src/commonMain/kotlin/legalbot/compose/app/composeApp/di/InitKoin.kt
+++ compose-app/src/commonMain/kotlin/legalbot/compose/app/composeApp/di/InitKoin.kt
@@ -1,5 +1,6 @@
 package legalbot.compose.app.composeApp.di
 
+import compose.app.shared.di.sharedModule
 import legalbot.user.onboarding.di.userOnboardingModule
 import org.koin.core.context.startKoin
 import org.koin.dsl.KoinAppDeclaration
@@ -7,6 +8,6 @@ import org.koin.dsl.KoinAppDeclaration
 fun initKoin(config: KoinAppDeclaration? = null) {
     startKoin {
         config?.invoke(this)
-        modules(userOnboardingModule)
+        modules(sharedModule, userOnboardingModule)
     }
 }
