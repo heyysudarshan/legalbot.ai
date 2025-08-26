b6fa1d8009956939ad6695a1da0e0cb333908d1d
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-08-26 14:17:43 +0530

    Add `Main.kt` entry point for `web-app` module with Compose Viewport initialization

---
+++ web-app/src/wasmJsMain/kotlin/legalbot/web/app/Main.kt
@@ -0,0 +1,16 @@
+package legalbot.web.app
+
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.window.ComposeViewport
+import kotlinx.browser.document
+import legalbot.compose.app.WebApp
+import legalbot.compose.app.composeApp.di.initKoin
+
+@OptIn(ExperimentalComposeUiApi::class)
+fun main() {
+    val webApp = document.getElementById("web-app")!!
+    ComposeViewport(viewportContainer = webApp) {
+        initKoin()
+        WebApp()
+    }
+}
