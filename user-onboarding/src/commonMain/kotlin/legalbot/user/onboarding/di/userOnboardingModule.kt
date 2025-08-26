44d107eeb0cc0b5f3a1c9a1d38e2b48a9a8658bc
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-08-26 12:06:24 +0530

    Refactor use case imports to align with updated package structure

--- user-onboarding/src/commonMain/kotlin/legalbot/user/onboarding/di/userOnboardingModule.kt
+++ user-onboarding/src/commonMain/kotlin/legalbot/user/onboarding/di/userOnboardingModule.kt
@@ -8,8 +8,8 @@ import legalbot.user.onboarding.repository.language.LanguageRepositoryImpl
 import legalbot.user.onboarding.repository.theme.ThemeRepositoryImpl
 import legalbot.user.onboarding.repository.language.LanguageRepository
 import legalbot.user.onboarding.repository.theme.ThemeRepository
-import legalbot.user.onboarding.domain.useCase.LanguageUseCase
-import legalbot.user.onboarding.domain.useCase.ThemeUseCase
+import legalbot.user.onboarding.domain.useCase.language.LanguageUseCase
+import legalbot.user.onboarding.domain.useCase.theme.ThemeUseCase
 import legalbot.user.onboarding.presentation.languagePage.LanguagePageViewModel
 import legalbot.user.onboarding.presentation.themePage.ThemePageViewModel
 import org.koin.core.module.dsl.singleOf
