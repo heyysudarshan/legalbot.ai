6f66a1be838f83e807dbb7c2b4b87eafbacf31a2
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-08-28 00:27:28 +0530

    Convert `LocaleManager` to an `object` and update `setLocale` to `provides` with `staticCompositionLocalOf`.

--- shared/src/desktopMain/kotlin/compose/app/shared/util/LocaleManager.desktop.kt
+++ shared/src/desktopMain/kotlin/compose/app/shared/util/LocaleManager.desktop.kt
@@ -1,10 +1,29 @@
 package compose.app.shared.util
 
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.ProvidedValue
+import androidx.compose.runtime.staticCompositionLocalOf
 import java.util.Locale
 
 @Suppress("EXPECT_ACTUAL_CLASSIFIERS_ARE_IN_BETA_WARNING")
-actual class LocaleManager {
-    actual fun setLocale(locale: String) {
-        Locale.setDefault(Locale.of(locale))
+actual object LocaleManager {
+    private var defaultLocale: Locale? = null
+    private var localAppLocale = staticCompositionLocalOf { Locale.getDefault().toString() }
+
+    @Composable
+    actual infix fun provides(value: String?): ProvidedValue<*> {
+        if (defaultLocale == null) {
+            defaultLocale = Locale.getDefault()
+        }
+
+        val newLocale = when (value) {
+            null -> defaultLocale!!
+            else -> {
+                Locale.forLanguageTag(value)
+            }
+        }
+
+        Locale.setDefault(newLocale)
+        return localAppLocale.provides(newLocale.toString())
     }
 }
