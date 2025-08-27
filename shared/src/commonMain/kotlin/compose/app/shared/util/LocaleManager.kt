12b6afd3387d2f2b0fe21eb4dd6e398b24bb4ba4
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-08-28 00:24:59 +0530

    Fix formatting in `LocaleManager` by adjusting annotations for consistency

--- shared/src/commonMain/kotlin/compose/app/shared/util/LocaleManager.kt
+++ shared/src/commonMain/kotlin/compose/app/shared/util/LocaleManager.kt
@@ -6,6 +6,6 @@ import androidx.compose.runtime.ProvidedValue
 @Suppress("EXPECT_ACTUAL_CLASSIFIERS_ARE_IN_BETA_WARNING")
 expect object LocaleManager {
     val current: String @Composable get
-    @Composable infix fun provides(value: String?): ProvidedValue<*>
-    fun updateLocale(locale: String)
+    @Composable
+    infix fun provides(value: String?): ProvidedValue<*>
 }
