c536471f50651a7a3f2026db66d19b079ff85db3
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-08-27 16:40:25 +0530

    Add rounded corners and hover pointer icon to `ThemeCard` for improved UI feedback

--- user-onboarding/src/commonMain/kotlin/legalbot/user/onboarding/presentation/themePage/component/ThemeCard.kt
+++ user-onboarding/src/commonMain/kotlin/legalbot/user/onboarding/presentation/themePage/component/ThemeCard.kt
@@ -9,17 +9,21 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.input.pointer.PointerIcon
+import androidx.compose.ui.input.pointer.pointerHoverIcon
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import compose.app.shared.domain.model.ThemeType
 import compose.app.shared.presentation.theme.ComposeAppTheme
 import legalbot.user.onboarding.domain.model.theme.Theme
-import legalbot.user.onboarding.domain.model.theme.ThemeType
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @Composable
@@ -32,8 +36,10 @@ internal fun ThemeCard(
     val backgroundModifier = modifier
         .padding(horizontal = 20.dp)
         .background(color = MaterialTheme.colorScheme.surface)
+        .clip(shape = RoundedCornerShape(size = 12.dp))
         .clickable(onClick = onClick)
         .padding(vertical = 12.dp)
+        .pointerHoverIcon(icon = PointerIcon.Hand)
 
     Row(verticalAlignment = Alignment.CenterVertically, modifier = backgroundModifier) {
         RadioButton(selected = theme.isSelected, onClick = onClick)
