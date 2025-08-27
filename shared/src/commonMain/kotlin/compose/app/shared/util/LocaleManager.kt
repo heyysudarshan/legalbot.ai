221e91ce1030e4500b5d22016c6e09eaca5bf392
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-08-28 00:24:15 +0530

    Convert `LocaleManager` to an `object` and add `current` and `provides` as composable properties

--- shared/src/commonMain/kotlin/compose/app/shared/util/LocaleManager.kt
+++ shared/src/commonMain/kotlin/compose/app/shared/util/LocaleManager.kt
@@ -1,6 +1,11 @@
 package compose.app.shared.util
 
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.ProvidedValue
+
 @Suppress("EXPECT_ACTUAL_CLASSIFIERS_ARE_IN_BETA_WARNING")
-expect class LocaleManager {
+expect object LocaleManager {
+    val current: String @Composable get
+    @Composable infix fun provides(value: String?): ProvidedValue<*>
     fun updateLocale(locale: String)
 }
