ecc63461733356dff5da12247c84b7df59a7926b
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-08-27 14:29:33 +0530

    Add rounded corners and hover pointer icon to `LanguageCard` for improved UI feedback

--- user-onboarding/src/commonMain/kotlin/legalbot/user/onboarding/presentation/languagePage/components/LanguageCard.kt
+++ user-onboarding/src/commonMain/kotlin/legalbot/user/onboarding/presentation/languagePage/components/LanguageCard.kt
@@ -9,12 +9,16 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.input.pointer.PointerIcon
+import androidx.compose.ui.input.pointer.pointerHoverIcon
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import compose.app.shared.presentation.theme.ComposeAppTheme
@@ -31,8 +35,10 @@ internal fun LanguageCard(
     val backgroundModifier = modifier
         .padding(horizontal = 20.dp)
         .background(color = MaterialTheme.colorScheme.surface)
+        .clip(shape = RoundedCornerShape(size = 12.dp))
         .clickable(onClick = onClick)
         .padding(vertical = 12.dp)
+        .pointerHoverIcon(icon = PointerIcon.Hand)
 
     Row(verticalAlignment = Alignment.CenterVertically, modifier = backgroundModifier) {
         RadioButton(selected = language.isSelected, onClick = onClick)
