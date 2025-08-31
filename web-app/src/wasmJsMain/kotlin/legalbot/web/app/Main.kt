f66d4e58fb20fec5b21242d79b2b43f89055a828
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-08-31 23:23:35 +0530

    Refactor `Main.kt` to utilize `onWasmReady` and enable browser navigation binding for navigation hosts

--- web-app/src/wasmJsMain/kotlin/legalbot/web/app/Main.kt
+++ web-app/src/wasmJsMain/kotlin/legalbot/web/app/Main.kt
@@ -1,15 +1,30 @@
 package legalbot.web.app
 
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.window.ComposeViewport
+import androidx.navigation.ExperimentalBrowserHistoryApi
+import androidx.navigation.bindToBrowserNavigation
 import kotlinx.browser.document
 import legalbot.compose.app.WebApp
-import legalbot.compose.app.composeApp.di.initKoin
+import legalbot.compose.app.di.initKoin
+import org.jetbrains.skiko.wasm.onWasmReady
 
-@OptIn(ExperimentalComposeUiApi::class)
+@OptIn(ExperimentalComposeUiApi::class, ExperimentalBrowserHistoryApi::class)
 fun main() {
-    ComposeViewport(viewportContainer = document.body!!) {
-        initKoin()
-        WebApp()
+    initKoin()
+    onWasmReady {
+        val body = document.body ?: return@onWasmReady
+        ComposeViewport(viewportContainer = body) {
+            WebApp(
+                onAllNavHostsReady = { navHostControllers ->
+                    navHostControllers.forEach { navHostController ->
+                        LaunchedEffect(Unit) {
+                            navHostController.bindToBrowserNavigation()
+                        }
+                    }
+                }
+            )
+        }
     }
 }
