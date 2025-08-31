4cec61e9098f9810abd3231f65a05b8023cd3bd0
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-08-31 23:20:30 +0530

    Introduce `customAppLocale` mutable state for app-wide locale customization

---
+++ shared/src/commonMain/kotlin/compose/app/shared/presentation/composeApp/customAppLocale.kt
@@ -0,0 +1,7 @@
+package compose.app.shared.presentation.composeApp
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+
+var customAppLocale by mutableStateOf<String?>(null)
