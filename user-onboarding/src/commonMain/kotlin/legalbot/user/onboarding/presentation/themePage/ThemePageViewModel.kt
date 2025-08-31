48329ea352cb9d7d59536e43d938e3160edf4749
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-08-31 23:28:58 +0530

    Link `ThemePageViewModel` with `ComposeAppViewModel` to synchronize theme updates and adjust dark mode settings

--- user-onboarding/src/commonMain/kotlin/legalbot/user/onboarding/presentation/themePage/ThemePageViewModel.kt
+++ user-onboarding/src/commonMain/kotlin/legalbot/user/onboarding/presentation/themePage/ThemePageViewModel.kt
@@ -2,13 +2,18 @@ package legalbot.user.onboarding.presentation.themePage
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import compose.app.shared.presentation.composeApp.ComposeAppViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import legalbot.user.onboarding.domain.model.theme.Theme
+import compose.app.shared.domain.model.ThemeType
 import legalbot.user.onboarding.domain.useCase.theme.ThemeUseCase
 
-internal class ThemePageViewModel(private val themeUseCase: ThemeUseCase) : ViewModel() {
+internal class ThemePageViewModel(
+    private val themeUseCase: ThemeUseCase,
+    private val composeAppViewModel: ComposeAppViewModel
+) : ViewModel() {
     val uiState = MutableStateFlow(value = ThemePageUiState())
 
     init {
@@ -23,5 +28,13 @@ internal class ThemePageViewModel(private val themeUseCase: ThemeUseCase) : View
 
     fun updateSelectedTheme(theme: Theme) {
         themeUseCase.updateSelectedTheme(theme = theme)
+        when (theme.type) {
+            ThemeType.Light -> {
+                composeAppViewModel.updateDarkTheme(isDarkMode = false)
+            }
+            else -> {
+                composeAppViewModel.updateDarkTheme(isDarkMode = true)
+            }
+        }
     }
 }
