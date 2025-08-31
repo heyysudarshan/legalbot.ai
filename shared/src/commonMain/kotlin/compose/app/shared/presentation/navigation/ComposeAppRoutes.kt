76033a4004269a98f1f93474802ff26ebb7e07d8
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-08-31 23:19:06 +0530

    Annotate `UserOnboardingModule` route with `@SerialName` for serialization consistency

--- shared/src/commonMain/kotlin/compose/app/shared/presentation/navigation/ComposeAppRoutes.kt
+++ shared/src/commonMain/kotlin/compose/app/shared/presentation/navigation/ComposeAppRoutes.kt
@@ -1,8 +1,9 @@
 package compose.app.shared.presentation.navigation
 
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 sealed class ComposeAppRoutes {
-    @Serializable
+    @Serializable @SerialName("user-onboarding")
     object UserOnboardingModule : ComposeAppRoutes()
 }
