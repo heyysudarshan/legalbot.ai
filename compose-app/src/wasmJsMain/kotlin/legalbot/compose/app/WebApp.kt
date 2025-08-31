85e1351fbc04f768fd7359efe83a565fbfbb68be
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-08-31 23:23:12 +0530

    Add `onAllNavHostsReady` callback to `WebApp` and pass it to `ComposeApp`

--- compose-app/src/wasmJsMain/kotlin/legalbot/compose/app/WebApp.kt
+++ compose-app/src/wasmJsMain/kotlin/legalbot/compose/app/WebApp.kt
@@ -1,9 +1,10 @@
 package legalbot.compose.app
 
 import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
 import legalbot.compose.app.presentation.ComposeApp
 
 @Composable
-fun WebApp() {
-    ComposeApp()
+fun WebApp(onAllNavHostsReady: @Composable (List<NavHostController>) -> Unit) {
+    ComposeApp(onAllNavHostsReady = onAllNavHostsReady)
 }
